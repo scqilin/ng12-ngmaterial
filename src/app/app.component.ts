c7f035b4b031d0f3f3e4f3827f17df8d931c21d5
scqilin <sql1211@qq.com>
2021-12-15 10:51:05 +0800

    翼星求生

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,7 +29,7 @@ export interface DialogData {
 })
 export class AppComponent {
   title = '猎人：荒野的召唤';
-  imgs = [ "1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg","9.jpg","10.jpg","11.jpg","12.jpg"]
+  imgs = [ "1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg","9.jpg","10.jpg","11.jpg","12.jpg","a1.jpg","a2.jpg","a3.jpg","a4.jpg","a5.jpg","a7.jpg","a7.jpg",]
   constructor(public dialog: MatDialog) {}
   openDialog(imgurl:string) {
     this.dialog.open(DialogElementsExampleDialog,{
